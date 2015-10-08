46007714941ce69093e81c5811cb396e879a75a1
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-10-08 01:41:12 +0100

    playing only once

--- zoidbot_tools/scripts/demo.py
+++ zoidbot_tools/scripts/demo.py
@@ -90,41 +90,42 @@ class JointPlayer(object):
         keys = lines[0].rstrip().split(',')
     
         l = 0
-        # If specified, repeat the file playback 'loops' number of times
-        while loops < 1 or l < loops:
-            i = 0
-            l += 1
-            print("Moving to start position...")
-    
-            _cmd, lcmd_start, rcmd_start, _raw = self.clean_line(lines[1], keys)
-            left.move_to_joint_positions(lcmd_start)
-            right.move_to_joint_positions(rcmd_start)
-            start_time = rospy.get_time()
-            for values in lines[1:]:
-                i += 1
-                loopstr = str(loops) if loops > 0 else "forever"
-                sys.stdout.write("\r Record %d of %d, loop %d of %s" %
-                                 (i, len(lines) - 1, l, loopstr))
-                sys.stdout.flush()
-    
-                cmd, lcmd, rcmd, values = self.clean_line(values, keys)
-                #command this set of commands until the next frame
-                while (rospy.get_time() - start_time) < values[0]:
-                    if rospy.is_shutdown():
-                        print("\n Aborting - ROS shutdown")
-                        return False
-                    if len(lcmd):
-                        left.set_joint_positions(lcmd)
-                    if len(rcmd):
-                        right.set_joint_positions(rcmd)
-                    if ('left_gripper' in cmd and
-                        grip_left.type() != 'custom'):
-                        grip_left.command_position(cmd['left_gripper'])
-                    if ('right_gripper' in cmd and
-                        grip_right.type() != 'custom'):
-                        grip_right.command_position(cmd['right_gripper'])
-                    rate.sleep()
-            print
+#        # If specified, repeat the file playback 'loops' number of times
+#        while loops < 1 or l < loops:
+#            i = 0
+        l += 1
+        print("Moving to start position...")
+        i = 0
+        _cmd, lcmd_start, rcmd_start, _raw = self.clean_line(lines[1], keys)
+        left.move_to_joint_positions(lcmd_start)
+        right.move_to_joint_positions(rcmd_start)
+        start_time = rospy.get_time()
+        for values in lines[1:]:
+            i += 1
+            loopstr = str(loops) if loops > 0 else "forever"
+            sys.stdout.write("\r Record %d of %d, loop %d of %s" %
+                             (i, len(lines) - 1, l, loopstr))
+            sys.stdout.flush()
+
+            cmd, lcmd, rcmd, values = self.clean_line(values, keys)
+            #command this set of commands until the next frame
+            while (rospy.get_time() - start_time) < values[0]:
+                if rospy.is_shutdown():
+                    print("\n Aborting - ROS shutdown")
+                    return False
+                if len(lcmd):
+                    left.set_joint_positions(lcmd)
+                if len(rcmd):
+                    right.set_joint_positions(rcmd)
+                if ('left_gripper' in cmd and
+                    grip_left.type() != 'custom'):
+                    grip_left.command_position(cmd['left_gripper'])
+                if ('right_gripper' in cmd and
+                    grip_right.type() != 'custom'):
+                    grip_right.command_position(cmd['right_gripper'])
+                rate.sleep()
+            print "This is done:"
+        print "DONEEEE"
         return True
 
 
@@ -282,7 +283,6 @@ class BaxInputRead(object):
         self.limbs=[]
         for i in self.limbnames:
             self.limbs.append(BaxIOinterface(i))
-
         self.rs.enable()
         
         t = Timer(1.0, self.readinginps)
